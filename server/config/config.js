9cb76b2b476e2a3e059aa285b02a9d4c5778fbd5
Jesper <jsp_hansen@outlook.com>
2019-04-02 10:40:50 +0700

    updated config

--- server/config/config.js
+++ server/config/config.js
@@ -4,15 +4,15 @@ const config = {
   PORT: process.env.PORT || SERVER_PORT,
   PRODUCTION: process.env.NODE_ENV === 'production',
   DATABASE: process.env.NODE_ENV === 'production'
-    ? 'mongodb://seng:hashdb_admin@178.62.73.101/hashpaydb'
+    ? ''
     : 'mongodb://localhost/hashpaydb',
   STRIPE: {
     PUBLISH_KEY: process.env.NODE_ENV === 'production'
-      ? 'pk_test_ZbLym6LFKkbNowX0vXSq5NKH'
-      : 'pk_test_ZbLym6LFKkbNowX0vXSq5NKH',
+      ? 'pk_test_xxx'
+      : 'pk_test_xxx',
     SECRET_KEY: process.env.NODE_ENV === 'production'
-      ? 'sk_test_3NEvIuGkcWmvdauB22t6F3hy'
-      : 'sk_test_3NEvIuGkcWmvdauB22t6F3hy'
+      ? 'pk_test_xxx'
+      : 'pk_test_xxx'
   }
 };
 
